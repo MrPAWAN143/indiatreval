7255e57c8aeed64329c5942cefdefed907a749a0
MrPAWAN143 <pkumarrana143@gmail.com>
2024-05-30 18:24:23 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>homepage</title>
+    <link rel="stylesheet" href="./assest/files/css/style.css">
+    <link rel="stylesheet" href="./assest/files/css/style2.css">
+</head>
+
+<body>
+   <header class="w-[100%] h-[80px] bg-[#fffafa]">
+    <nav class="md:w-[1260px] md:m-auto h-[100%] md:flex">
+        <div class="logo md:w-[15%] md:flex md:items-center md:justify-center">
+       <a href=""> <img src="./assest/images/logo-big-1.png" alt="main logo"></a>
+        </div>
+        <div class="search md:w-[20%] leading-[40px] flex items-center">
+            <div class="search-box w-[100%] h-[40px] m-auto border flex justify-evenley items-center ">
+                <input type="search" name="" id="" class="w-[100%] h-[100%] " placeholder="Where do you want to go?">
+
+            </div>
+        </div>
+        <div class="menu md:w-[65%] leading-[65px] ml-3 mr-2">
+            <ul class="flex gap-1 items-center px-5">
+                <li><a href="#" class="p-2">Group Tours</a></li>
+                <li><a href="#" class="p-2">Speciality Tours</a></li>
+                <li><a href="#" class="p-2">Experiences</a></li>
+                <li><a href="#" class="p-2">Tailormade Holidays</a></li>
+                <li><a href="#" class="p-2">Deals</a></li>
+                <li><a href="#" class="p-2">More</a></li>
+            </ul>
+
+
+        </div>
+    </nav>
+   </header>
+</body>
+
+</html>
