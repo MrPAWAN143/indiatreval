7c54a0c5b4beccd8127f524e725a6529b7fc4f42
MrPAWAN143 <pkumarrana143@gmail.com>
2024-07-18 13:06:09 +0530

    category section created

--- index.php
+++ index.php
@@ -21,8 +21,7 @@
             </div>
             <div class="search md:w-[20%] leading-[40px] flex items-center">
                 <div class="search-box w-[100%] h-[40px] m-auto border flex justify-evenley items-center ">
-                    <input type="search" name="" id="" class="w-[100%] h-[100%] "
-                        placeholder="Where do you want to go?">
+                    <input type="search" name="" id="" class="w-[100%] h-[100%] " placeholder="Where do you want to go?">
 
                 </div>
             </div>
@@ -49,7 +48,7 @@
         </nav>
     </header>
 
-    <div class="mainsection w-[1200px] m-auto">
+    <div class="mainsection w-full m-auto">
 
         <section class="slider_section relative">
 
@@ -62,8 +61,7 @@
 
 
                 <div class="item relative">
-                    <div
-                        class="contentOnherosection absolute left-[50%] z-10  translate-x-[-50%] top-[40%] translate-y-[-50%]">
+                    <div class="contentOnherosection absolute left-[50%] z-10  translate-x-[-50%] top-[40%] translate-y-[-50%] object-cover">
                         <h2 class="text-[70px] font-serif text-green-700">Rajasthan</h2>
                     </div>
                     <img src="assest/images/pexels-sbsoneji-3581369.jpg" alt="">
@@ -80,7 +78,7 @@
 
             </div>
 
-            <div class="sliderContent">
+            <div class="sliderContent w-[1200px] m-auto">
                 <p class="w-[89%] m-auto spacialpara text-[17px]">Rajasthan, the land of Kings and India’s largest
                     desert
                     land, is an incredible northern Indian state that
@@ -104,17 +102,104 @@
 
 
 
-        <section class="border border-red-500 min-h-[650px] mt-28 w-full">
+        <section class="border border-red-500 min-h-[650px] mt-28 w-[1200px] m-auto">
             <div class="cityheadline ml-20">
-                <h2 class="text-[30px] font-bold">TOUR IN <span
-                        class="underline underline-offset-[6px] decoration-[5px] decoration-yellow-300">RAJASTHAN</span>
+                <h2 class="text-[30px] font-bold">TOUR IN <span class="underline underline-offset-[6px] decoration-[5px] decoration-yellow-300">RAJASTHAN</span>
                 </h2>
             </div>
 
 
 
             <div class="allSection w-full h-full mt-4 flex items-center">
-                <div class="filterSection w-[25%] min-h-[650px] border border-cyan-400"></div>
+                <div class="filterSection w-[25%] min-h-[650px] border border-cyan-400">
+
+                    <div class="filterAndReset w-[80%] m-auto flex justify-between items-center h-auto px-3 py-4 border-b border-b-black">
+                        <a href=""> <span class="text-black font-medium text-[22px]">Filters</span></a>
+                        <a href=""><span class="text-gray-600 font-normal text-[20px]">Reset</span></a>
+
+                    </div>
+                    <div class="filterSection  w-[80%] m-auto  h-auto px-3 mt-4">
+                        <div class="categorySection relative">
+                            <div class="categories flex items-center justify-between">
+                                <h3 class="text-black-500 font-normal text-[22px] underline underline-offset-[6px] decoration-yellow-500 decoration-[3px]">Categories</h3>
+                            </div>
+                            <div class="checkboxes flex flex-col min-h-[200px] mt-2">
+                                <label for="Honeymoon" class="flex justify-start items-center py-[3px] text-[18px] font-normal w-[90%] ">
+                                    <input type="checkbox" name="Honeymoon" id="Honeymoon" class="mr-5 checkbox_items">
+                                    Honeymoon
+                                </label>
+
+
+                                <label for="family" class="flex justify-start items-center py-[3px] text-[18px] font-normal w-[90%]">
+                                    <input type="checkbox" name="family" id="family" class="mr-5 checkbox_items">
+                                    Family
+                                </label>
+
+
+                                <label for="friend" class="flex justify-start items-center py-[3px] text-[18px] font-normal w-[90%] ">
+                                    <input type="checkbox" name="friend" id="friend" class="mr-5 checkbox_items" >
+                                    Friends / Group
+                                </label>
+
+                                <span class="clear">CLEAR</span>
+
+                            </div>
+                        </div>
+                        <div class="categorySection relative">
+                            <div class="categories flex items-center justify-between">
+                                <h3 class="text-black-500 font-normal text-[22px] underline underline-offset-[6px] decoration-yellow-500 decoration-[3px]">Categories</h3>
+                            </div>
+                            <div class="checkboxes flex flex-col min-h-[200px] mt-2">
+                                <label for="Honeymoon" class="flex justify-start items-center py-[3px] text-[18px] font-normal w-[90%] ">
+                                    <input type="checkbox" name="Honeymoon" id="Honeymoon" class="mr-5 checkbox_items">
+                                    Honeymoon
+                                </label>
+
+
+                                <label for="family" class="flex justify-start items-center py-[3px] text-[18px] font-normal w-[90%]">
+                                    <input type="checkbox" name="family" id="family" class="mr-5 checkbox_items">
+                                    Family
+                                </label>
+
+
+                                <label for="friend" class="flex justify-start items-center py-[3px] text-[18px] font-normal w-[90%] ">
+                                    <input type="checkbox" name="friend" id="friend" class="mr-5 checkbox_items" >
+                                    Friends / Group
+                                </label>
+
+                                <span class="clear">CLEAR</span>
+
+                            </div>
+                        </div>
+                        <div class="categorySection relative">
+                            <div class="categories flex items-center justify-between">
+                                <h3 class="text-black-500 font-normal text-[22px] underline underline-offset-[6px] decoration-yellow-500 decoration-[3px]">Categories</h3>
+                            </div>
+                            <div class="checkboxes flex flex-col min-h-[200px] mt-2">
+                                <label for="Honeymoon" class="flex justify-start items-center py-[3px] text-[18px] font-normal w-[90%] ">
+                                    <input type="checkbox" name="Honeymoon" id="Honeymoon" class="mr-5 checkbox_items">
+                                    Honeymoon
+                                </label>
+
+
+                                <label for="family" class="flex justify-start items-center py-[3px] text-[18px] font-normal w-[90%]">
+                                    <input type="checkbox" name="family" id="family" class="mr-5 checkbox_items">
+                                    Family
+                                </label>
+
+
+                                <label for="friend" class="flex justify-start items-center py-[3px] text-[18px] font-normal w-[90%] ">
+                                    <input type="checkbox" name="friend" id="friend" class="mr-5 checkbox_items" >
+                                    Friends / Group
+                                </label>
+
+                                <span class="clear">CLEAR</span>
+
+                            </div>
+                        </div>
+                       
+                    </div>
+                </div>
                 <div class="outputSection w-[55%] min-h-[650px] border border-gray-400"></div>
                 <div class="suggestionSection w-[20%] min-h-[650px] border border-red-400"></div>
             </div>
@@ -135,7 +220,6 @@
 
 
     <script>
-
         $('.slider').slick({
             dots: true,
             infinite: true,
